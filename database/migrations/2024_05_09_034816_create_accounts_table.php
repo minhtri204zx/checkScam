29617ce5c8363fff757911b913f39765e01635d2
Tri Nguyen <trinmph36953@fpt.edu.vn>
2024-05-09 16:51:08 +0700

    5/9/2024 start

---
+++ database/migrations/2024_05_09_034816_create_accounts_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Models\Role;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('username');
+            $table->string('email');
+            $table->string('password');
+            $table->string('name');
+            $table->string('linkfb');
+            $table->string('numberphone');
+            $table->text('bank');
+            $table->string('describe');
+            $table->string('cmnd');
+            $table->string('games');
+            $table->integer('money')->default(0); 
+            $table->integer('numcomments')->default(3);
+            $table->string('active')->nullable();
+            $table->foreignIdFor(Role::class)->nullable()->constrained(); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
